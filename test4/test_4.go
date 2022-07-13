2f57127bc8ea385ee20e25c045fccc93b1f56fbb
oktagondev_fariz <fariz@oktagon.id>
2022-07-13 08:35:01 +0700

    Skill Test Kawan Lama

--- test4/test_4.go
+++ test4/test_4.go
@@ -11,58 +11,59 @@ func main() {
 	var hasilPermata float64
 	var hasil float64
 	var jenis string
-	var ruby = 10  // Didapatkan dari Combinasi 5
-	var topaz = 6 // Didapatkan dari Combinasi 3
+	var ruby = 5  
+	var topaz = 3 
 	var permata = 1
 	var pRuby = 1000000
 	var pTopaz = 1250000
 	var pPermata = 3000000
+	var x1, x2, x3, x4, x5, x6, x7, x8, x9 = 1, 1, 1, 1, 1, 1, 1, 1, 1
+	var kombinasiRuby float64
+	var kombinasiTopaz float64
+	var kombinasiPermata float64
 
-	//var input string
-	// var inputan float64
-	// var x1, x2, x3 = 1, 1, 1
-	// var kombinasi float64
+	// kombinasi ruby
+	for i := 1; i <= int(ruby); i++ {
+		x1 = x1 * i
+	}
+	for i := 1; i <= 2; i++ {
+		x2 = x2 * i
+	}
+	for i := 1; i <= int(ruby-2); i++ {
+		x3 = x3 * i
+	}
+	kombinasiRuby = float64(x1) / (float64(x2) * float64(x3))
+	fmt.Println(kombinasiRuby)
 
-	//Bila ingin Menghitung Kombinasi Menggunakan Program
+	// kombinasi Topaz
+	for i := 1; i <= int(topaz); i++ {
+		x4 = x4 * i
+	}
+	for i := 1; i <= 2; i++ {
+		x5 = x5 * i
+	}
+	for i := 1; i <= int(topaz-2); i++ {
+		x6 = x6 * i
+	}
+	kombinasiTopaz = float64(x4) / (float64(x5) * float64(x6))
+	fmt.Println(kombinasiTopaz)
 
-	// fmt.Println("Input the jerewely Ruby/Permata/Topaz:")
-	// fmt.Scan(&input)
+	// kombinasi Permata
+	for i := 1; i <= int(permata); i++ {
+		x7 = x7 * i
+	}
+	for i := 1; i <= 2; i++ {
+		x8 = x8 * i
+	}
+	for i := 1; i <= int(permata-2); i++ {
+		x9 = x9 * i
+	}
+	kombinasiPermata = float64(x7) / (float64(x8) * float64(x9))
+	fmt.Println(kombinasiPermata)
 
-	// if input == "Ruby" {
-	// 	inputan = float64(ruby)
-	// } else if input == "Topaz" {
-	// 	inputan = float64(topaz)
-	// } else {
-	// 	inputan = float64(permata)
-	// }
-	// for i := 1; i <= int(inputan); i++ {
-	// 	x1 = x1 * i
-	// }
-	// for i := 1; i <= 2; i++ {
-	// 	x2 = x2 * i
-	// }
-	// for i := 1; i <= int(inputan-2); i++ {
-	// 	x3 = x3 * i
-	// }
-	// kombinasi = float64(x1) / (float64(x2) * float64(x3))
-	// fmt.Println(kombinasi)
-
-	// if input == "Ruby" {
-	// 	hasilRuby = float64(pRuby * int(kombinasi))
-	// 	jenis = "Ruby"
-	// } else if input == "Topaz" {
-	// 	hasilTopaz = float64(pTopaz * int(kombinasi))
-	// 	jenis = "Topaz"
-	// } else {
-	// 	hasilPermata = float64(pPermata * int(kombinasi))
-	// 	jenis = "Permata"
-	// }
-
-	hasilRuby = float64(pRuby) * float64(ruby)
-	hasilTopaz = float64(pTopaz) * float64(topaz)
-	hasilPermata = float64(pPermata) * float64(permata)
-
-	fmt.Println(hasilRuby, hasilTopaz, hasilPermata)
+	hasilRuby = float64(pRuby) * kombinasiRuby
+	hasilTopaz = float64(pTopaz) * kombinasiTopaz
+	hasilPermata = float64(pPermata) * kombinasiPermata
 
 	//Membandingkan hasil dari harga kombinasi yang dihasilkan
 	hasil = math.Max(hasilRuby, hasilTopaz)
@@ -74,6 +75,6 @@ func main() {
 	} else {
 		jenis = "Permata"
 	}
-	var kesimpulan string = fmt.Sprintf("Jumlah Keuntungan Maksimal yang didapat dengan menjual jenis %s Adalah : %v ",jenis, hasil )
+	var kesimpulan string = fmt.Sprintf("Jumlah Keuntungan Maksimal yang didapat dengan menjual jenis %s, dengan keuntungan : %v ", jenis, hasil)
 	fmt.Printf(kesimpulan)
 }
